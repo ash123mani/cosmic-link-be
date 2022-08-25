81a938da8fc3b8ff6c4deb2fa6b10adc3bc01417
Ashutosh Mani Tripathi <ashutosh.tripathi@walmart.com>
2022-08-25 20:56:13 +0530

    Added healthcheck

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use("/api/v1/auth", require("./routes/auth"));
 app.use("/api/v1/link", require("./routes/link"));
 app.use("/api/v1/user", require("./routes/user"));
+app.use('/healthcheck', require('./routes/healthcheck'));
 
 // Error Handler (Should be last piece of middleware)
 app.use(errorHandler);
