572f88d66067ac882aef1ab275fcf79c9762a7ea
Ashutosh <ashuma2721@gmail.com>
2023-02-27 21:29:54 +0530

    updated the env vars

--- middleware/resp-headers.js
+++ middleware/resp-headers.js
@@ -1,5 +1,5 @@
 const responseHeaders = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
